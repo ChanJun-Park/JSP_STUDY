5646caaa14f1f6378269f280d54b767d3fcc53e9
ChanJun-Park <pcj01142@gmail.com>
2019-08-21 15:51:44 +0900

    spring 연습

---
+++ pro19/src/com/spring/ex02/PersonServiceImpl.java
@@ -0,0 +1,24 @@
+package com.spring.ex02;
+
+public class PersonServiceImpl implements PersonService {
+	private String name;
+	private int age;
+	
+	public PersonServiceImpl(String name) {
+		super();
+		this.name = name;
+	}
+	public PersonServiceImpl(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public void sayHello() {
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age);
+	}
+}
